d6af72b138dfe94f348743e0b221ebc7ceedac2b
Elma Rahman <elmarahman635@gmail.com>
2019-05-02 19:19:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,7 +67,7 @@ def checkout(cart, coupons) #must use whats called in arg.
         final_cart = apply_clearance(couponed_cart) #finalcart calling apply_clearance method.
           cart_total = 0
             final_cart.each do |item, properties|
-                cart_total += properties[:price] * properties[:count]
+                cart_total += properties[:price] * properties[:count] #mult. price with count of properties in cart
               end
                     cart_total = [:price] * 0.80 > 100
 
