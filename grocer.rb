4370edad9997ecba1199432eeefa01b3f39e4a82
Elma Rahman <elmarahman635@gmail.com>
2019-05-02 19:29:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,4 +71,5 @@ def checkout(cart, coupons) #must use whats called in arg.
               end
                   #10% 100> #should just be cart_total bc thats what we're apply disc. to & not [:price]
                   cart_total = cart_total * 0.90 if cart_total > 100
+                    cart_total
           end
